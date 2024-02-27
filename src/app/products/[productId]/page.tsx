2d1854e00983fed9ae9a6f8ac7d016d4895f005d
scamgi <scampini.giacomo@gmail.com>
2024-02-27 16:59:40 +0100

    metadata modified

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,5 +1,28 @@
-export default function ProductDetails({ params }: {
-    params: { productId: string }
-}) {
+import { Metadata } from "next";
+
+type Props = {
+    params: {
+        productId: string;
+    }
+};
+
+// export const generateMetadata = ({ params }: Props): Metadata => {
+//     return {
+//         title: `Product ${params.productId}`
+//     }
+// }
+
+export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
+    const title = await new Promise(resolve => {
+        setTimeout(() => {
+            resolve(`iPhone ${params.productId}`)
+        }, 100);
+    });
+    return {
+        title: `Product ${title}`
+    };
+};
+
+export default function ProductDetails({ params }: Props) {
     return <h1>Details about product {params.productId}</h1>;
 }
