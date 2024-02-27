f2db65a726407960dee9f67f765077ab05b2b5bb
scamgi <scampini.giacomo@gmail.com>
2024-02-27 16:05:10 +0100

    layout modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header style={{backgroundColor:"lightblue", padding:"1rem"}}>
+          <p>Header</p>
+        </header>
+        {children}
+        <footer style={{backgroundColor:"ghostwhite", padding:"1rem"}}>
+          <p>Footer</p>
+        </footer>
+      </body>
     </html>
   );
 }
