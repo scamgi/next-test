c49b68e3bcd06100ab25e41d40d445b3e9e772b6
scamgi <scampini.giacomo@gmail.com>
2024-02-28 10:58:59 +0100

    links

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,22 @@
+import Link from "next/link";
+
 export default function ProductList() {
+    const productId = 100;
     return (
         <>
             <h1>Product list</h1>
-            <h2>Product 1</h2>
-            <h2>Product 2</h2>
-            <h2>Product 3</h2>
+            <h2>
+                <Link href="products/1">Product 1</Link>
+            </h2>
+            <h2>
+                <Link href="products/2">Product 2</Link>
+            </h2>
+            <h2>
+                <Link href="products/3">Product 3</Link>
+            </h2>
+            <h2>
+                <Link href={`products/${productId}`}>Product 100</Link>
+            </h2>
         </>
     );
 }
