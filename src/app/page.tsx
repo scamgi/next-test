9b208d1ce904a660bf19a711edbd1041365a49a9
scamgi <scampini.giacomo@gmail.com>
2024-02-28 11:41:58 +0100

    added loading.tsx to blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Home
       <Link href="about">About</Link>
+      <Link href="blog">Blog</Link>
     </main>
   );
 }
